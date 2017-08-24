4ecfb742e3dd603ab7f733bf11547ac23707d4ae
chenjj0703 <chenjj@chenjj-OptiPlex-3020>
2017-08-24 19:25:22 +0800

    4.4

--- charpter4/4.4.c
+++ charpter4/4.4.c
@@ -1,3 +1,11 @@
 	void copy_n(char dst[], char src[], int n)
 	{
-		
+		int dst_index, src_index;
+		src_index = 0;
+
+		for( det_index = 0; dst_index<n; dest_index++) {
+			dst[dest_index] = src [dest_index];
+				if ( src[src_index] != 0)
+					src_index ++;
+		}
+	} 	
